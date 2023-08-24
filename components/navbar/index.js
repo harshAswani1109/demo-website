c230729cbf50c0c5ca7c82d899ddfa646b3f521b
rajsrm2021 <rj9769@srmist.edu.in>
2023-08-24 23:34:17 +0530

    assets added

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -3,15 +3,12 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center h-20 px-4">
-      <h1>Hi im a Navbar</h1>
-      <div className="flex gap-4">
-        <Link href="/team">Team</Link>
-        <Link href="/vision">vision</Link>
-        <Link href="/sponsors">sponsors</Link>
-        <Link href="/career">career</Link>
-        <Link href="/about">about</Link>
+    <div >
+      <div>
+        hi i am navbar
+        Mr.Raj
       </div>
+      
     </div>
   );
 };
