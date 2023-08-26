75566b7f97aeb2e8d548a6763693f186f6dad4c6
Mayank Pathak <100159883+Mayankkpathak@users.noreply.github.com>
2023-08-26 18:23:31 +0530

    update

--- app/about/page.js
+++ app/about/page.js
@@ -4,13 +4,13 @@ import { GrNext } from "react-icons/gr";
 import Link from "next/link";
 const AboutPage =()=>{
   return(
-    <div className="container my-10 mx-5 md:mx-auto flex flex-col md:flex-row border-4 p-6 md:p-12 justify-between items-center border-black rounded-3xl space-y-10 md:space-x-40">
+    <div className="container my-10 md:mx-auto flex flex-col md:flex-row border-4 p-6 md:p-12 justify-between items-center border-black rounded-3xl space-y-10 md:space-x-40">
       <div>
         <Image src={srmImage} width={2500}/>
       </div>
       
       <div className="flex flex-col justify-between items-center space-y-10 md:space-y-20">
-        <div className="text-lg md:text-3xl text-justify">
+        <div className="text-lg md:text-3xl ">
         SRM Institute of Science and Technology (formerly known as SRM University) is one of the top ranking universities in India with over 38,000 students and more than 2600 faculty across all the campus, offering a wide range of undergraduate, postgraduate and doctoral programs in Engineering, Management, Medicine and Health sciences, and Science and Humanities.
         </div>
         <Link href="/career"  className="flex justify-end w-full">
