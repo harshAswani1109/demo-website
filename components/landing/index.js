01a72165a3fdc667ab91f9d8d8f575cbf9d0f87c
rajsrm2021 <rj9769@srmist.edu.in>
2023-08-30 16:40:21 +0530

    landing added and bug fixed

--- components/landing/index.js
+++ components/landing/index.js
@@ -10,8 +10,8 @@ const Landing = () => {
             "url('https://static.wixstatic.com/media/ffb6a1_1817a3625f5c458184e0e7254163328b~mv2.jpg/v1/fill/w_1280,h_708,al_c,q_85,enc_auto/ffb6a1_1817a3625f5c458184e0e7254163328b~mv2.jpg')",
         }}
       >
-        <section className="text-center">
-          <h1 className="text-5xl font-bold mb-4 text-blue-700 hover:text-blue-900 transition-colors duration-300 transform hover:scale-105">
+        <section className="text-center z-0" >
+          <h1 className="text-5xl font-bold mb-4 text-blue-700 hover:text-blue-900 transition-colors duration-300  hover:scale-105 z-0">
             THE <span className="text-yellow-500">ELECRUISERS</span>
           </h1>
 
@@ -20,7 +20,7 @@ const Landing = () => {
           </p>
         </section>
       </main>
-      <div className="flex flex-wrap justify-center items-center gap-10 ">
+      <div className="flex flex-wrap justify-center items-center gap-10 mt-20">
         <More />
       </div>
     </>
